25c6853459c568c565e358eb9de8e37365af760a
Himanshu Sharma <ranubhardwaj89@gmail.com>
2020-05-18 17:26:37 +0530

    Bot Deploy

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 const Twit = require('twit');
-
-const T = new Twit({
-  consumer_key: 'PZOQa3EXsefINWzszUpeP5sGZ',
-  consumer_secret: 'St5bGY1aYwfZ7Wn06YIQAVDGHiAGulpReLhVqRIrUmMrDgL5Z2',
-  access_token: '1260450633259384833-qN2QJfDOd4PZ8BL3653KEM30cUNCzF',
-  access_token_secret: 'DM3HOnPZyc0ivcfTLPr8VZF0RYyMpIatBHFrEEjsMxVpR'
-});
+const auth = require('./config');
+const T = new Twit(auth);
 
 // start stream and track tweets
 const stream = T.stream('statuses/filter', { track: '#100DaysOfFlutter' });
