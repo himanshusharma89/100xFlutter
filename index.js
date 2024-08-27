bab1c2875d4ad6aeedfdb68af83aaa3c42adcb6f
Himanshu Sharma <contact@himanshusharma.tech>
2024-08-27 23:15:31 +0530

    package and logic update

--- index.js
+++ index.js
@@ -5,15 +5,28 @@ const T = new Twit(auth);
 // start stream and track tweets
 const stream = T.stream('statuses/filter', { track: '#100DaysOfFlutter' });
 
-// use this to log errors from requests
+// Helper to handle errors and avoid rate limits
 function responseCallback(err, data, response) {
-  console.log(err);
+  if (err) {
+    console.error('Error:', err.message);
+  } else {
+    console.log('Action performed:', data);
+  }
 }
 
-// event handler
+// Event handler to process tweets
 stream.on('tweet', tweet => {
-  // retweet
-  T.post('statuses/retweet/:id', { id: tweet.id_str }, responseCallback);
-  // like
-  T.post('favorites/create', { id: tweet.id_str }, responseCallback);
+  try {
+    // Check if the tweet is from a bot or self to avoid unnecessary actions
+    if (tweet.user.bot || tweet.user.screen_name === auth.own_account_name) return;
+
+    // Retweet
+    T.post('statuses/retweet/:id', { id: tweet.id_str }, responseCallback);
+
+    // Like
+    T.post('favorites/create', { id: tweet.id_str }, responseCallback);
+
+  } catch (err) {
+    console.error('Processing Error:', err.message);
+  }
 });
